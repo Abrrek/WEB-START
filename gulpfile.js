8ca927dc76a13b374ab0fcfb1ed36a33c3c138fa
Movsar <movsar.kh.128@gmail.com>
2020-03-05 20:37:10 +0300

    feat: added gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,36 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
+const sass = require('gulp-sass');
 
-gulp.task('gulp', function(done) {
-	console.log('Привет, мир!');
-});
-
-gulp.task('browser-sync', function() {
+function bs() {
+	serveSass();
 	browserSync.init({
 			server: {
 					baseDir: "./"
 			}
 	});
-	gulp.watch("./*.html").on('change', browserSync.reload);
-});
+	watch("./*.html").on('change', browserSync.reload);
+	watch("./sass/**/*.sass", serveSass);
+	watch("./js/*.js").on('change', browserSync.reload);
+};
 
-gulp.task('cssnano', function() {
-	return gulp.src('./css/*.css')
+function css() {
+	return src('./css/*.css')
 			.pipe(cssnano())
 			.pipe(rename({
 				suffix: ".min"
 			}))
-			.pipe(gulp.dest('./css'));
-});
+			.pipe(dest('./css'));
+};
+
+function serveSass() {
+	return src('./sass/**/*.sass')
+    .pipe(sass())
+		.pipe(dest('./css'))
+		.pipe(browserSync.stream());
+};
+
+
+exports.serve = bs;

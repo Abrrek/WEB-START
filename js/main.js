d7015fc2c900c43783e466d48eb1c9e4f1d2a7b7
Movsar <movsar.kh.128@gmail.com>
2020-03-12 11:17:18 +0300

    feat: Added closing of the block by clicking on an empty space and pressing on the space

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	const modal = document.querySelector('.modal');
 	const modalBtn = document.querySelectorAll('[data-toggle=modal]');
 	const closeBtn = document.querySelector('.modal__close');
+	const closeModal = document.querySelector('.modal__close-block');
 	const switchModal = () => {
 		modal.classList.toggle('modal--visible');
 	}
@@ -11,4 +12,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	});
 
 	closeBtn.addEventListener('click', switchModal);
+	closeModal.addEventListener('click', switchModal);
+	document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+				switchModal();
+    }
+}
 });
